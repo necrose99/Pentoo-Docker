caebaae476e5962f1f1bc83f82027487092d714e
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-11-29 23:36:31 -0600

    Update provision.sh

--- pentoo-x86-default/provision.sh
+++ pentoo-x86-default/provision.sh
@@ -1,18 +1,29 @@
 # This one should be present by ning the build.sh script
-echo "Getting power ISO to Unpack Pentoo into the Gentoo root & Overlay"
-wget -O poweriso-1.3.tar.gz http://goo.gl/p8Tzc
-tar -xzvf poweriso-1.3.tar.gz -C /usr/local/bin
-chmod +x /usr/local/bin/poweriso
-mkdir /pentoo_tmp
-cd  /pentoo_tmp
+
+mkdir newWorldOrder; cd newWorldOrder ; mkdir /newWorldOrder/pentoo_tmp; cd  /newWorldOrder/pentoo_tmp
 wget -bqc http://www.pentoo.ch/isos/latest-iso-symlinks/pentoo-x86-default.iso
-echo "pentoo-amd64-default.iso unpacking"
-./poweriso extract pentoo-amd64-default.iso / -od /pentoo_tmp
-emerge -v sys-fs/squashfs-tools
+7z x *.iso
+echo "pentoo-x86-default.iso unpacking"
+rm *.iso
 echo "unpacking Squashfile and modules"
-unsquashfs -f -d / /pentoo_tmp/modules/image.squashfs
-unsquashfs -f -d / /pentoo_tmp/modules/*.lzm* 
-rm -rf /pentoo_tmp/
-eslect profile set pentoo:pentoo/hardened/linux/x86/bleeding_edge
+unsquashfs -f -d /newWorldOrder/  /newWorldOrder/pentoo_tmp/modules/image.squashfs
+unsquashfs -f -d /newWorldOrder/ /newWorldOrder/pentoo_tmp/modules/*.lzm* 
+rm -rf /newWorldOrder/pentoo_tmp/
+
+#Busy Box
+wget -q -O /busybox "http://www.busybox.net/downloads/binaries/latest/busybox-${busybox_version}"
+chmod +x /busybox
+/busybox rm -rf /lib* /usr /var /bin /sbin /opt /mnt /media /root /home /run /tmp
+/busybox cp -fRap lib* /
+/busybox cp -fRap bin boot home media mnt opt root run sbin tmp usr var /
+/busybox cp -fRap etc/* /etc/
+cd /
+#commit suicide 
+/busybox rm -rf newWorldOrder /busybox /provision.sh /linuxrc 
+#Add sync portage
+emerge-webrsync -q 
+layman -S
+# update pentoo.
+eslect profile set pentoo:pentoo/default/linux/x86/bleeding_edge
 pentoo-updater
-echo "Bootstrapped  Pentoo iso into /:"
+echo "Bootstrapped  Pentoo iso into /  & Happy Hacking...."
